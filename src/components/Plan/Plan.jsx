9d21a4f7dcf00f6fa2e121e7bb437636beeaaf6c
Ilvina Rakhimova <ilvrakh@gmail.com>
2022-11-27 23:06:43 +0500

    small fix

--- src/components/Plan/Plan.jsx
+++ src/components/Plan/Plan.jsx
@@ -7,12 +7,13 @@ function Plan(props) {
   let planClass = classNames(style.plan, props.isSelected ? style.selected : "")
   return (
         <div className={planClass}>
-            <h2 className={style.main_info} styleName={props.colorOne}>Безлимитный {props.price}</h2>
-            <div className={style.main_info} styleName={props.colorTwo}>руб <span className={style.price}>{props.price}</span>/мес</div>
-            <div className={style.plan__speed}>до {props.speed} Мбит/сек</div>
-            <div className={style.plan__info}>{props.info}</div>
+            <h2 className={style.details} styleName={props.colorOne}>Безлимитный {props.price}</h2>
+            <div className={style.details} styleName={props.colorTwo}>руб <span className={style.price}>{props.price}</span>/мес</div>
+            <div className={style.speed}>до {props.speed} Мбит/сек</div>
+            <div className={style.info}>{props.info}</div>
         </div>
   )
 }
 
 export default CSSModules(Plan, style)
+

579f459909b5fd2a819dc906f84564e3e5577f1d
Ilvina Rakhimova <ilvrakh@gmail.com>
2022-12-02 22:05:00 +0500

    added state for cards class

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,30 @@
+import { useState } from 'react';
 import style from './app.module.scss';
 import Plan from '../Plan/Plan.jsx';
 import data from '../../data/data.json';
 
 
 function App() {
+  const [counter, setCounter] = useState();
+
   return (
     <div className={style.app}>
-      <div className={style.plans}>
     {
       data.map((plan) =>
       <Plan
-      key={plan.id}
-      colorOne={plan.colorOne} 
-      colorTwo={plan.colorTwo} 
-      price={plan.price} 
-      speed={plan.speed}
-      info={plan.info}
-      // isSelected={plan.isSelected}
-      >  
-      </Plan>
+      key = {plan.id}
+      id = {plan.id}
+      colorOne = {plan.colorOne} 
+      colorTwo ={ plan.colorTwo} 
+      price = {plan.price} 
+      speed = {plan.speed}
+      info = {plan.info}
+      isSelected = {plan.id === counter}
+      counter = {counter}
+      setCounter = {setCounter}
+      />  
       )
     }
-      </div>
     </div>
   );
 }
--- src/components/Plan/Plan.jsx
+++ src/components/Plan/Plan.jsx
@@ -1,25 +1,26 @@
-import { useState } from 'react';
 import style from './plan.module.scss';
 import CSSModules from 'react-css-modules';
 import classNames from 'classnames';
 
 
-function Plan(props) {
-  const [selected, setSelected] = useState(false);
-  const handleChange = () => {
-    setSelected (!selected);
+function Plan({id, colorOne, colorTwo, price, speed, info, isSelected, setCounter}) {
+  let planClass = classNames(style.plan, isSelected ? style.selected : "")
+
+  const getId = () => {
+    if (isSelected) {
+      return setCounter()
+    }
+    return setCounter(id)
   }
 
-  // let planClass = classNames(style.plan, props.isSelected ? style.selected : "")
   return (
-        <div onClick = {handleChange} className={classNames(style.plan, selected ? style.selected : "")}>
-          <h2 className={style.details} styleName={props.colorOne}>Безлимитный {props.price}</h2>
-          <div className={style.details} styleName={props.colorTwo}>руб <span className={style.price}>{props.price}</span>/мес</div>
-          <div className={style.speed}>до {props.speed} Мбит/сек</div>
-          <div className={style.info}>{props.info}</div>
+        <div onClick = {getId} className={planClass}>
+          <h2 className={style.details} styleName={colorOne}>Безлимитный {price}</h2>
+          <div className={style.details} styleName={colorTwo}>руб <span className={style.price}>{price}</span>/мес</div>
+          <div className={style.speed}>до {speed} Мбит/сек</div>
+          <div className={style.info}>{info}</div>
         </div>
   )
 }
 
 export default CSSModules(Plan, style)
-
